05589f9fb78164ee859b7c8f7b2e0b9ba3bce590
Carlos Madrigal <madwaro@live.com>
2019-02-22 20:21:14 -0800

    Disabling logging

--- src/commands.js
+++ src/commands.js
@@ -3,7 +3,8 @@ import sketch from 'sketch'
 export default function () {
 	const document = sketch.getSelectedDocument()
 	if (!document) {
-		// During developing, every time I save the script executes with a null document.
+		// During developing, every time I save the script executes with a null
+		// document.
 
 		return
 	}
@@ -12,13 +13,9 @@ export default function () {
 	const layers = page.layers
 	const layerCount = layers.length
 
-	if (page.layers.length == 0) {
-		toast('No layers in this page. Done!')
-
-		return
+	if (page.layers.length > 0) {
+		rearrange(page)
 	}
-
-	rearrange(page)
 
 	toast('Done')
 }
@@ -28,12 +25,12 @@ function rearrange(page) {
 
 	// Create a copy of the array so we can work on it in Javascript
 	var layers = []
-	page.layers.forEach((a) => {
-		layers.push(a)
+	page.layers.forEach((l) => {
+		layers.push(l)
 	})
 
-	// Sort layers according to their x, y coordinates. Top left corner wins and then
-	// it goes into a Z pattern.
+	// Sort layers according to their x, y coordinates. Top left corner wins and
+	// then it goes into a Z pattern.
 	layers.sort(function (a, b) {
 		const aX = a.frame.x
 		const aY = a.frame.y
@@ -44,15 +41,27 @@ function rearrange(page) {
 		const deltaY = aY - bY
 
 		let result = 0
+
+		// When both layers are on the same column, then sort based on their
+		// y-position
 		if (deltaX == 0) {
 			result = deltaY
-		} else if (deltaY == 0) {
+		}
+		// When both layers are on the same row, then sort based on their x-position
+		else if (deltaY == 0) {
 			result = deltaX
-		} else if (deltaX < 0 && deltaY < 0) {
+		}
+		// When layer a is before in both x & y axis, it goes first
+		else if (deltaX < 0 && deltaY < 0) {
 			result = -2
-		} else if (deltaY <0) {
+		}
+		// When layer b is after layer in the x-axis, but before in the y-axis, it
+		// goes first
+		else if (deltaY < 0) {
 			result = -1
-		} else {
+		}
+		// Otherwise, it goes later
+		else {
 			result = 1
 		}
 
@@ -61,22 +70,23 @@ function rearrange(page) {
 		return result
 	})
 
-	// Iterate the sorted array from the bottom and move each layer to the front. At the end of, everything will be sorted.
+	// Iterate the sorted array from the bottom and move each layer to the front.
+	// At the end of, everything will be sorted.
 	for (var i=layers.length-1; i >=0; i--) {
 		let layer = layers[i]
 		let oldIndex = layer.index
 
 		layer.moveToFront()
 
-		log(`${layer.name} (${layer.frame.x}, ${layer.frame.y}), oldIndex:${oldIndex}, newIndex:${i}`)
+		log(`${layer.name} (${layer.frame.x}, ${layer.frame.y})`)
 	}
 }
 
 function log(text) {
-	// Uncomment the following line to enable logs. 
+	// Uncomment the following line to enable logs.
 	// Open Console and filter entries using "Mise en place"
 
-	console.log(`Mise en place: ${text}`)
+	// console.log(`Mise en place: ${text}`)
 }
 
 function toast(text) {
